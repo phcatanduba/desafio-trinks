a31ebe523ade5a3f0daae5df576a53eedc44098c
Pedro Henrique <phcatanduba@gmail.com>
2021-08-31 15:42:53 -0300

    feature: lucky number function

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,28 @@ function isAHappyNumber(number: string, index = 1): boolean {
 }
 
 function isALuckyNumber(number: string) {
-    return true;
+    let listOfNumbers: number[] = [];
+    for (let i = 1; i <= +number; i++) {
+        if (i % 2 !== 0) {
+            listOfNumbers.push(i);
+        }
+    }
+
+    let index = 0;
+    while (listOfNumbers.length > listOfNumbers[index]) {
+        index++;
+        console.log(listOfNumbers);
+        listOfNumbers = listOfNumbers.filter(
+            (number, j) => (j + 1) % listOfNumbers[index] !== 0
+        );
+        console.log(listOfNumbers);
+    }
+
+    if (listOfNumbers.includes(+number)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 input.question(
